c7a94a00683421245e3f1ccb35700f05e305698c
Danny Hirsch <byakkoroyale@gmail.com>
2026-07-01 21:49:05 +0200

    Added QR code lib. Adjusted frontend. Added post method for scan path to backend

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -2,6 +2,9 @@ import express, { Request, Response } from 'express';
 import path from 'path';
 
 const app = express();
+
+app.use(express.json());
+
 const PORT = 3000;
 
 const frontendPath = path.join(__dirname, '../frontend');
@@ -9,6 +12,13 @@ app.use(express.static(frontendPath));
 
 app.get('/api/status', (req: Request, res: Response) => {
     res.json({ message: "Backend API Test" });
+});
+
+app.post('/api/scan', (req: Request, res: Response) => {
+    const receivedBarcode = req.body.barcode;
+    console.log("Backend hat einen Barcode empfangen:", receivedBarcode);
+
+    res.json({ message: "Barcode erfolgreich im Backend angekommen!", code: receivedBarcode });
 });
 
 app.listen(PORT, () => {
